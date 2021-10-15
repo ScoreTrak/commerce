b020f75d3d18ae431f7ea52ccb16d23fdd8fd3d5
L1ghtman2k <aibek.zhylkaidarov@hpe.com>
2021-10-15 04:58:17 -0400

    fix static path

--- backend/ldfinance/settings.py
+++ backend/ldfinance/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = 'static'
-STATIC_URL = '/static/'
+STATIC_URL = '/opt/app/static'
 
 
 AUTH_USER_MODEL = 'users.User'
